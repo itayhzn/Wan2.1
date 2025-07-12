a9f4045ba3c8f3ffa578e6afde6dc13d375deccd
itayhzn <itay.hzn@gmail.com>
2025-07-12 18:54:19 +0300

    example of using SAM2

--- main.py
+++ main.py
@@ -1,41 +1,67 @@
 import os
+# if using Apple MPS, fall back to CPU for unsupported ops
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+import numpy as np
 import torch
-from datetime import datetime
+import matplotlib.pyplot as plt
+from PIL import Image
+import cv2
 
-def read_videojam_prompts():
+device = torch.device("cuda")
+torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
+if torch.cuda.get_device_properties(0).major >= 8:
+    torch.backends.cuda.matmul.allow_tf32 = True
+    torch.backends.cudnn.allow_tf32 = True
+
+from sam2.sam2_video_predictor import SAM2VideoPredictor
+
+predictor = SAM2VideoPredictor.from_pretrained("facebook/sam2-hiera-large")
+
+def read_video(video_path):
     """
-    Read prompts from the videojam_prompts.txt file.
-    Returns a list of prompts.
+    Read mp4 video and return a list of frames as numpy arrays.
     """
-    with open("videojam_prompts.txt", "r") as f:
-        prompts = f.readlines()
-    return [prompt.strip() for prompt in prompts if prompt.strip()]
+    video = []
+    cap = cv2.VideoCapture(video_path)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+        video.append(frame)
+    cap.release()
+    return video
+
+def show_mask(mask, ax, obj_id=None, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        cmap = plt.get_cmap("tab10")
+        cmap_idx = 0 if obj_id is None else obj_id
+        color = np.array([*cmap(cmap_idx)[:3], 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+
+def show_points(coords, labels, ax, marker_size=200):
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+
+
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0, 0, 0, 0), lw=2))
+
 
 if __name__ == "__main__":
-    experiment_name = "selfx2eqx1"
-
-    datetime_str = datetime.now().strftime("%Y%m%d_%H%M%S")
-
-    prompts = read_videojam_prompts()
-    [
-        "Two ibexes navigating a rocky hillside. They are walking down a steep slope covered in small rocks and dirt. In the background, there are more rocks and some greenery visible through an opening in the rocks.",
-        "Athletic man doing gymnastics elements on horizontal bar in city park. Male sportsmen perform strength exercises outdoors.",
-        "A small dog playing with a red ball on a hardwood floor.",
-        "A white kitten playing with a ball.",
-        "A woman enjoying the fun of hula hooping."
-    ]
-
-    seeds = [ '1024' ]
-
-    addit_prompt =  "A cat." # "A cat." # "A red octopus moving its tentacles around."
-
-    # redirect output to a file
-    with open(f"jobs-out-err/{datetime_str}_{experiment_name}.out", "w") as f:
-        os.dup2(f.fileno(), 1)
-    # redirect error output to a file
-    with open(f"jobs-out-err/{datetime_str}_{experiment_name}.err", "w") as f:
-        os.dup2(f.fileno(), 2)
-
-    os.system(f"""python generate.py --task t2v-1.3B --size 832*480 --ckpt_dir ./Wan2.1-T2V-1.3B --prompts "{'" "'.join(prompts)}" --seeds {' '.join(seeds)} --paired_generation "True" --addit_prompt "{addit_prompt}" --experiment_name "{experiment_name}" """)
-
-    # os.system(f"""python generate.py --task t2v-1.3B --size 832*480 --ckpt_dir ./Wan2.1-T2V-1.3B --prompts "{'" "'.join(prompts)}" --seeds {' '.join(seeds)} --paired_generation "True" --addit_prompt "{addit_prompt}" --experiment_name "{experiment_name}" """)
+    video_path = "generated/20250709_072402_attn_head_4_subject_mask_t2v-1.3B_832*480_1_1_A_small_brown_dog_playing_with_ADDIT_A_white_kitten.mp4"  # Path to your video file
+    video_frames = read_video(video_path)
+    
+    # save the first frame as an image
+    first_frame = video_frames[0]
+    first_frame_image = Image.fromarray(first_frame)
+    first_frame_image.save("generated/first_frame.jpg")
