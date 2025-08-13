ac820689eec6c230d8a08b2d12a870b3407eab7a
itayhzn <itay.hzn@gmail.com>
2025-08-13 14:10:05 +0300

    testing param passing

--- main.py
+++ main.py
@@ -21,6 +21,6 @@ if __name__ == "__main__":
     # with open("error.txt", "w") as f:
     #     os.dup2(f.fileno(), 2)
 
-    os.system("""python generate.py --task t2v-1.3B --size 832*480 --ckpt_dir ./Wan2.1-T2V-1.3B --prompts "A woman performing an intricate dance on stage, illuminated by a single spotlight in the first frame." --seeds 1024 """)
+    # os.system("""python generate.py --task t2v-1.3B --size 832*480 --ckpt_dir ./Wan2.1-T2V-1.3B --prompts "A woman performing an intricate dance on stage, illuminated by a single spotlight in the first frame." --seeds 1024 """)
 
-    # os.system("""python generate.py --task t2v-1.3B --size 832*480 --ckpt_dir ./Wan2.1-T2V-1.3B --edit_mode "True" --input_paths "datasets/woman_jumping_on_horse.mp4" --subject_prompts "the horse jumping" --edit_prompts "a blue zebra" --base_seed 1024 --experiment_name "samwise_mask" """)
+    os.system("""python generate.py --task t2v-1.3B --size 832*480 --ckpt_dir ./Wan2.1-T2V-1.3B --edit_mode "True" --input_paths "datasets/woman_jumping_on_horse.mp4" --subject_prompts "the horse jumping" --edit_prompts "a blue zebra" --base_seed 1024 --experiment_name "samwise_mask" """)
