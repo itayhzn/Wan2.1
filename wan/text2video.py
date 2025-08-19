322d072d3fb810b823d3f71a90d04fe740dd2bf0
itayhzn <itay.hzn@gmail.com>
2025-08-19 12:07:19 +0300

    t=45 and 50 steps

--- wan/text2video.py
+++ wan/text2video.py
@@ -305,7 +305,7 @@ class WanT2V:
             arg_null = {'context': context_null, 'seq_len': seq_len, 'edit_context': context_null, 'subject_mask': subject_mask, 'edit_mode': edit_mode}
 
             anchor_Zt = None
-            start_timestep = 49 if edit_mode else 0
+            start_timestep = 45 if edit_mode else 0
 
             noise = [
                 sample_scheduler.add_noise(
@@ -316,8 +316,8 @@ class WanT2V:
                 timestep = [t]
                 
                 if edit_mode:
-                    if idx < start_timestep:
-                        continue
+                    # if idx < start_timestep:
+                    #     continue
 
                     anchor_Zt = sample_scheduler.add_noise(
                         anchor_z0, noise[0], torch.tensor(timestep)) # [C, F, H, W]
