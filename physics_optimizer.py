8b9d4d0cdc5a44cecac6be4d0e4b30009bc4b9c9
itayhzn <itay.hzn@gmail.com>
2025-08-28 13:51:09 +0300

    bugfix

--- physics_optimizer.py
+++ physics_optimizer.py
@@ -126,7 +126,7 @@ class Optimizer:
                 noise_pred = noise_pred_uncond + self.guide_scale * (
                     noise_pred_cond - noise_pred_uncond)
 
-                losses = self.compute_loss(latent, timestep, timestep_idx, noise_pred)
+                losses = physics_invariants.compute_losses(latent, timestep, timestep_idx, noise_pred)
                 loss = losses[self.loss_name]
 
                 loss.backward(retain_graph=False)
